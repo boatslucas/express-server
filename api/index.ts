339bffc7e8ee50e07b6b5672f3b9b7128fa72364
boatslucas <raman.lucas@cccareers.org>
2025-06-27 16:06:40 -0700

    third committal

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,20 @@
-/*const express = require("express");*/
+const express = require("express");
 /*const app = express();*/
-const app = require('./app.ts');
+//const app = require('./app.ts');
+const app = express();
 
+const data = require('./data.json');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the API!');
+});
+app.get('/data', (req, res) => {
+  res.status(200).json(data);
+});
 /*app.get("/", (req, res) => res.send("Express on Vercel"));*/
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+//module.exports = app;
